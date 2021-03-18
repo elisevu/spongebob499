23705a69d5b716ad7bce6460e1ee8ce6022859f8
elisevu <44879175+elisevu@users.noreply.github.com>
2021-03-17 20:51:19 -0400

    dictionary with parts tagged as dialogue, character, episode title, and context

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import bleu
 import rouge
 import numpy as np
 import os
-
+import re   
 path = 'archive/SpongeBob_SquarePants_Transcripts'
 # print(os.path.exists(path))
 with open('all_spongebob.txt', 'w', encoding='utf8') as sponge:
@@ -22,6 +22,31 @@ with open('all_spongebob.txt', 'r', encoding='utf8') as sponge:
 separated = []
 for line in sponge_script:
     separated.append(line.split(': '))
+#tags characters, titles, dialogue and context parts
+tag = {}
+for line in separated:
+    for part in line:
+        if part == '':
+            break
+        if part.find('.txt') != -1:
+            tag[part] = 'EPISODE'
+            break
+        try:
+            context = re.findall(r"\[+.+\]", part)
+        except AttributeError:
+            context = ''
+        if part == line[0] and context == '':
+            tag[part] = 'CHAR'
+        else:
+            tag[tuple(context)] = 'CONTXT'
+            dialogue = ''
+            for each in context:
+                dialogue = dialogue + (part.replace(each, ''))
+                dialogue.strip()
+            tag[dialogue] = 'DIALOGUE'
+
+
+        
 # print(separated[0:5])
     
 
