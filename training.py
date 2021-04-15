3fa6a91f8a5e84759ef6dc3711c53dda42e5be0c
Stephan Saliba <stephan.saliba@gmail.com>
2021-04-14 21:23:54 -0400

    batching added

--- training.py
+++ training.py
@@ -1,19 +1,34 @@
-from collections import Counter
 import nltk
 
 with open('actual_both_dialogues.txt', 'r', encoding='utf8') as doc:
-    text = []
+    text_tokenized = []
     for line in doc.readlines():
         try:
             speaker, dialogue = line.split(': ', 1)
             dialogue = nltk.word_tokenize(dialogue.lower())
-            text.append(speaker.lower()+":") 
-            text += dialogue
+            text_tokenized.append(speaker.lower()+":") 
+            text_tokenized += dialogue
         except ValueError:
             print(line)
 
 
-# text = text.lower()
-# text_tokenized = nltk.word_tokenize(text)
+word_to_ix = {key:val for val,key in enumerate(set(text_tokenized))}
+ix_to_word = {val:key for key,val in word_to_ix.items()}
 
-token_to_int = dict(Counter(text_tokenized))
+int_script = [word_to_ix[word] for word in text_tokenized]
+
+
+from torch.utils.data import TensorDataset, DataLoader
+
+def batch_data(int_script, seq_length, batch_size):
+    f, t = [], []
+
+    # separating the speaker from the line
+    for i in range(len(int_script) - seq_length):
+        f.append(int_script[i : i+seq_length])
+        t.append(int_script[i+seq_length])
+
+    data_set = TensorDataset(torch.tensor(f, dtype=torch.long), torch.tensor(t, dtype=torch.long))
+    
+    return DataLoader(data_set, shuffle=True, batch_size=batch_size)
+
